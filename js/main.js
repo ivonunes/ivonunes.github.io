9814a0aac44bdf7019a95a8e4529a7cf9465ebd9
Ivo Nunes <ivonunes@me.com>
2019-09-28 20:10:29 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () { 
+    var avatar = document.querySelector('.avatar'),
+        text = document.querySelector('section'),
+        social = document.querySelector('.social'),
+        center = document.querySelector('.center'),
+        made = document.querySelector('h3');
+
+    avatar.className = 'avatar animated fadeInDown';
+    text.className = 'animated zoomIn';
+    social.className = 'social animated fadeInUp';
+    made.className = 'animated fadeInUp'
+    center.style.visibility = 'visible';
+    made.style.visibility = 'visible'
+};
